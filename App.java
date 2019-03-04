a63b5b76606098dbaf91868e738dc4f481b065fe
2uk3y <2uk3y@2uk3ys-MacBook-Pro.local>
2019-03-04 16:49:16 +0800

    1st day training

---
+++ App.java
@@ -0,0 +1,10 @@
+// main() berada 1 class khusus yg akan guna class-class yg lain
+
+public class App {
+	// entry point method
+	public static void main(String[] args) {
+		// create object
+		Car ferrari = new Car();
+		Car porsche = new Car();
+	}
+}
---
+++ Car.java
@@ -0,0 +1,16 @@
+// nama file dan nama class mesti sama
+// class name guna camel case i.e NamaSaya
+// 1 class berada dlm 1 file
+
+public class Car {
+	String color;
+	int numOfDoors;
+
+	void startEngine() {
+
+	}
+
+	void stopEngine() {
+		int tempCounter = 0;
+	}
+}
